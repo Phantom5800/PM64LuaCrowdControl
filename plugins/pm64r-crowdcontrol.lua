65617dfc65c45597a2ea18db434de70eb42f7e47
Phantom <phantom@phantom-games.com>
2025-06-20 19:18:16 -0700

    Stub functionality for what the ideas are

---
+++ plugins/pm64r-crowdcontrol.lua
@@ -0,0 +1,72 @@
+local plugin = {}
+
+plugin.name = "PM64R Crowd Control"
+plugin.author = "Phantom5800"
+plugin.settings = 
+{
+    { name='slowgoenabled', type='file', label='Slow Go Enabled' },
+    { name='sethp', type='file', label='Set HP Value'},
+    { name='setfp', type='file', label='Set FP Value'}
+}
+
+plugin.description =
+[[
+    Trigger ingame events for Paper Mario when specific files are created.
+]]
+
+-- called each frame
+function plugin.on_frame(data, settings)
+    -- check if Slow Go should be enabled
+    if settings.slowgoenabled then
+        foundfile = false
+        local fn, err = io.open(settings.slowgoenabled, 'r')
+        if fn ~= nil then
+            foundfile = true
+            fn:close()
+        end
+
+        if foundfile then
+            -- force enable slow go badge effect
+        else
+            -- force disable slow go badge effect
+        end
+    end
+
+    -- check if there is a SetHP file
+    if settings.sethp then
+        foundfile = false
+        hpvalue = 0
+        local fn, err = io.open(settings.sethp, 'r')
+        if fn ~= nil then
+            foundfile = true
+            hpvalue = fn:read("*number")
+            fn:close()
+        end
+
+        if foundfile then
+            if hpvalue > 0 then
+                -- set hp in player struct
+            end
+            os.remove(settings.sethp)
+        end
+    end
+
+    -- check if there is a SetFP file
+    if settings.setfp then
+        foundfile = false
+        fpvalue = 0
+        local fn, err = io.open(settings.setfp, 'r')
+        if fn ~= nil then
+            foundfile = true
+            fpvalue = fn:read("*number")
+            fn:close()
+        end
+
+        if foundfile then
+            -- set fp in player struct
+            os.remove(settings.setfp)
+        end
+    end
+end
+
+return plugin
